939c08322c354fe475863b4b3dca822e0230c5d8
huboqiang <huboqiang@gmail.com>
2015-06-02 15:51:08 +0800

    add information.

---
+++ add.sh
@@ -0,0 +1,44 @@
+git add .gitignore
+git commit -m "Ignore file."
+
+git add	__init__.py
+git commit -m "Initiated file."
+
+git add	bin/
+git commit -m "Executable files for running."
+git add	module00_StatInfo.py
+git commit -m "File for sample information."
+
+git add	module01_mapping_from_raw.py
+git commit -m "Module1, generate scripts for mapping."
+
+git add	module02_call_peaks.py
+git commit -m "Module2, generate scripts for peak calling (mouse)."
+
+git add	module02_call_peaks_human.py
+git commit -m "Module2, generate scripts for peak calling (human)."
+
+git add	module03_geneDensity.py
+git commit -m "Module3, calculate read density on genes/XXbp Tiles."
+
+git add	module04_ChIP_RNA_cor.py
+git commit -m "Module4, RNA ChIP correlation on Promoter/Genebody."
+
+git add	module_LoadSamp.py
+git commit -m "File for sample information."
+
+git add	module_Matrix.py
+git commit -m "File for Matrix."
+
+git add	module_PlotChIP_RNA.py
+git commit -m "File for Plotting, 1."
+
+git add	module_PlotRNA_RankedChip_gene.py
+git commit -m "File for Plotting, 2."
+
+git add	module_StatFunc.py
+git commit -m "Submodule for statistics."
+git add	module_refGene.py
+git commit -m "Submodule for refGene.txt."
+git add	module_running_jobs.py
+git commit -m "Submodule for job running."
