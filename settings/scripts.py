1e62b323a6db3b8c542377e62d5b519e949d0b5a
huboqiang <huboqiang@gmail.com>
2016-08-09 12:40:57 +0800

    length auto-generated in s07.2.

--- settings/scripts.py
+++ settings/scripts.py
@@ -13,7 +13,7 @@ import ChIP.settings.projpath         as m_proj
 class Scripts(m_proj.ProjInfo):
     def __init__(self):
         super(Scripts,self).__init__()
-    
+
 
     def define_files(self,ref):
         dir_db = "%s/%s" % (self.Database, ref)
@@ -61,7 +61,7 @@ $dix_bins_exe -b 1000 $dir_database/${ref}.fa.fai $dir_database/columns.1kb.bed
 cut -f 1-2 $dir_database/${ref}.fa.fai >$dir_database/${ref}.fa.len
         """)
         return l_sh_info
-    
+
     def db_03_RefGene(self):
         l_sh_info = []
         l_sh_info.append("ref=$1")
@@ -154,7 +154,7 @@ $bedtools_exe sort -i   $dir_database/chrom.bed >$dir_database/chrom.sort.bed
         l_sh_info.append("""
 $pl_exe $pl_QC --indir $in_dir --outdir $out_dir --sample $samp             \\
     --end $data_type""")
-    
+
         return l_sh_info
 
     def s02_bwa(self):
@@ -197,7 +197,7 @@ if [ $end == "2" ]
       rm ${bam_prefix}/*sai ${bam_prefix}/${brief_name}.sam
 fi""")
         return l_sh_info
-        
+
     def s03_bam2bedrep(self):
         l_sh_info = []
         l_sh_info.append("brief_name=$1")
@@ -216,11 +216,11 @@ $java_exe -Xmx1g -jar $markDup_jar                                          \\
     INPUT=$bam/$brief_name/$brief_name.bam                                  \\
     OUTPUT=$bam/$brief_name/$brief_name.dedup.bam                           \\
     METRICS_FILE=$bam/$brief_name/$brief_name.picard_info.txt               \\
-    REMOVE_DUPLICATES=true ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT      
-                                                                                 
+    REMOVE_DUPLICATES=true ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT
+
 $python_exe $py_ExtRead -l $ext_len -p $end                                 \\
-    $bam/$brief_name/$brief_name.dedup.bam $bed_rep/$brief_name                  
-                                                                                 
+    $bam/$brief_name/$brief_name.dedup.bam $bed_rep/$brief_name
+
 $samtools_exe view -b -F 1548 -q 30 $bam/$brief_name/$brief_name.dedup.bam |\\
 $bedtools_exe bamtobed -i -                                                |\\
     awk 'BEGIN{FS="\\t";OFS="\\t"}{$4="N"; print $0}'                       \\
@@ -228,7 +228,7 @@ $bedtools_exe bamtobed -i -                                                |\\
 
 rm $bam/$brief_name/$brief_name.bam""")
         return l_sh_info
-    
+
     def s04_bedmrg(self):
         l_sh_info = []
         l_sh_info.append("bed_mrg=$1")
@@ -259,9 +259,9 @@ for i in $bed_mrg $@
 do
     sh $sh_sortbed $i.tagAlign $i.sort.tagAlign $bgzip_exe $tabix_exe
 done""")
-        
+
         return l_sh_info
-    
+
     def s04_2_bedmrg_multi(self):
         l_sh_info = []
         l_sh_info.append("bed_mrg=$1")
@@ -282,7 +282,7 @@ do
 done""")
         return l_sh_info
 
-    
+
     def s05_1_spp_rep_shiftSize(self):
         l_sh_info = []
         l_sh_info.append("samp=$1")
@@ -291,15 +291,15 @@ done""")
         l_sh_info.append("bed_rep_dir=%s" % self.dir_bed_rep)
         l_sh_info.append("spp_rep_dir=%s" % self.dir_spp_rep_shiftSize)
         l_sh_info.append("""
-rm $spp_rep_dir/$samp/out/out.tab                                           
-     
+rm $spp_rep_dir/$samp/out/out.tab
+
 $R_exe $spp_exe -c=$bed_rep_dir/$samp.sort.tagAlign.gz                      \\
     -odir=$spp_rep_dir/$samp/test -savp -rf                                 \\
     -out=$spp_rep_dir/$samp/out/out.tab""")
-      
+
         return l_sh_info
-    
-    
+
+
     def s05_2_spp_mrg_shiftSize(self):
         l_sh_info = []
         l_sh_info.append("merge=$1")
@@ -315,7 +315,7 @@ $R_exe $spp_exe -c=$bed_mrg_dir/$merge.sort.tagAlign.gz                     \\
     -out=$spp_mrg_dir/$merge/out/out.tab""")
 
         return l_sh_info
-        
+
     def s06_1_macs2PeakRep(self,ref):
         """ ref, effective genome size. It can be 1.0e+9 or 1000000000,
             or shortcuts:'hs' for human (2.7e9), 'mm' for mouse
@@ -353,14 +353,14 @@ $py_exe $macs2_exe callpeak -t $treat_bed.pr2.tagAlign.gz -c $ctrl_bed      \\
     -f BED -n ${name_prefix}.pr2_VS_Input -g $ref -p $pvalue --to-large     \\
     --nomodel --extsize $shift_size -B""")
         return l_sh_info
-        
+
 
     def s06_2_macs2PeakMrg(self,ref):
         """ ref, effective genome size. It can be 1.0e+9 or 1000000000,
             or shortcuts:'hs' for human (2.7e9), 'mm' for mouse
             (1.87e9), 'ce' for C. elegans (9e7) and 'dm' for
             fruitfly (1.2e8), Default:hs """
-        
+
         l_sh_info = []
         l_sh_info.append("merge_name=$1")
         l_sh_info.append("control=$2")
@@ -372,7 +372,7 @@ $py_exe $macs2_exe callpeak -t $treat_bed.pr2.tagAlign.gz -c $ctrl_bed      \\
         l_sh_info.append("dir_Peak_mrg=%s"   % self.dir_Peak_mrg)
         l_sh_info.append("get_psudoCount=%s" % self.sftw_get_psudoCount)
         l_sh_info.append("ref=%s"            % ref)
-        
+
         l_sh_info.append("""
 treat_bed=${dir_bed_mrg}/${merge_name}.sort.tagAlign.gz
 ctrl_bed=${dir_bed_mrg}/${control}.sort.tagAlign.gz
@@ -381,19 +381,19 @@ name_prefix=${dir_Peak_mrg}/${merge_name}/${merge_name}
 $py_exe $macs2_exe callpeak -t $treat_bed                 -c $ctrl_bed      \\
     -f BED -n ${name_prefix}_VS_Input     -g $ref -p $pvalue --to-large     \\
     --nomodel --extsize $shift_size -B
-    
+
 sh $get_psudoCount $treat_bed
 
 $py_exe $macs2_exe callpeak -t $treat_bed.pr1.tagAlign.gz -c $ctrl_bed      \\
     -f BED -n ${name_prefix}.pr1_VS_Input -g $ref -p $pvalue --to-large     \\
     --nomodel --extsize $shift_size -B
-    
+
 $py_exe $macs2_exe callpeak -t $treat_bed.pr2.tagAlign.gz -c $ctrl_bed      \\
     -f BED -n ${name_prefix}.pr2_VS_Input -g $ref -p $pvalue --to-large     \\
     --nomodel --extsize $shift_size -B""")
         return l_sh_info
-        
-    
+
+
     def s07_1_IDR_prepare(self):
         l_sh_info = []
         l_sh_info.append("mrg_peak=$1")
@@ -414,11 +414,11 @@ do
    sort -k 8nr,8nr $Peak_rep_dir/${i}/${i}_VS_Input_peaks.narrowPeak      | \\
        head -n     $top_peak   |  $bgzip_exe -cf                            \\
        >$Peak_rep_dir/${i}/${i}_VS_Input_peaks.regionPeak.gz             && \\
-   
+
    sort -k 8nr,8nr $Peak_rep_dir/${i}/${i}.pr1_VS_Input_peaks.narrowPeak  | \\
         head -n     $top_peak  |  $bgzip_exe -cf                            \\
        >$Peak_rep_dir/${i}/${i}.pr1_VS_Input_peaks.regionPeak.gz         && \\
-   
+
    sort -k 8nr,8nr $Peak_rep_dir/${i}/${i}.pr2_VS_Input_peaks.narrowPeak  | \\
         head -n     $top_peak  |  $bgzip_exe -cf                            \\
        >$Peak_rep_dir/${i}/${i}.pr2_VS_Input_peaks.regionPeak.gz         && \\
@@ -446,7 +446,7 @@ mv ${prefix}.pr1_VS_Input_peaks.regionPeak.gz                               \\
 mv ${prefix}.pr2_VS_Input_peaks.regionPeak.gz                               \\
     $Peak_idr_dir/${mrg_peak}""")
         return l_sh_info
-        
+
     def s07_2_usingMacs2Peak(self):
         l_sh_info = []
         l_sh_info.append("in_sam1=$1")
@@ -457,7 +457,12 @@ mv ${prefix}.pr2_VS_Input_peaks.regionPeak.gz                               \\
         l_sh_info.append("batchIDR_exe=%s"    % self.sftw_batchIDR)
         l_sh_info.append("genome_table=%s.len"% self.genome_ref       )
         l_sh_info.append("idr_dir=%s"         % self.dir_Peak_idr )
-        l_sh_info.append("""        
+        l_sh_info.append("""
+if [ ! -f $genome_table ]
+then
+    awk '{print $1 "\\t" $2}' %s.fai >$genome_table
+fi
+
 region1=$idr_dir/$merge_name/${in_sam1}_VS_Input_peaks.regionPeak.gz
 region2=$idr_dir/$merge_name/${in_sam2}_VS_Input_peaks.regionPeak.gz
 out=$idr_dir/$merge_name/$subtype/${in_sam1}_VS_${in_sam2}
@@ -465,10 +470,10 @@ out=$idr_dir/$merge_name/$subtype/${in_sam1}_VS_${in_sam2}
 $R_exe $batchIDR_exe $region1 $region2                                      \\
   -1  $out 0 F p.value $genome_table""")
         return l_sh_info
-        
+
     def s07_3_IDR_passPeaks(self):
         l_sh_info = []
-        
+
         l_sh_info.append("merge_name=$1")
         l_sh_info.append("peak=$2")
         l_sh_info.append("dir_Peak_idr=%s" % (self.dir_Peak_idr) )
@@ -486,7 +491,7 @@ zcat $infile | sort -k7nr,7nr | head -n $peak |                             \\
     $bgzip_exe -cf >$outfile                                             && \\
     $tabix_exe -f -p bed -s 1 -b 2 -e 3 -S 0 $outfile""")
         return l_sh_info
-        
+
     def s08_macs2BroadPeakRep(self,ref="hs"):
         """ ref, effective genome size. It can be 1.0e+9 or 1000000000,
             or shortcuts:'hs' for human (2.7e9), 'mm' for mouse
@@ -510,10 +515,10 @@ out_dir=${dir_broad_rep}/${brief_name}
 
 $py_exe $macs2_exe callpeak  --broad  -t $treat_bed       -c $ctrl_bed      \\
   -f BED -n ${brief_name}_VS_Input --outdir $out_dir -g $ref -p $pvalue -B""")
-        
+
         return l_sh_info
-        
-    
+
+
     def s09_macs2BroadPeakMrg(self,ref="hs"):
         """ ref, effective genome size. It can be 1.0e+9 or 1000000000,
             or shortcuts:'hs' for human (2.7e9), 'mm' for mouse
@@ -536,10 +541,10 @@ out_dir=${dir_broad_mrg}/${merge_name}
 
 $py_exe $macs2_exe callpeak  --broad  -t $treat_bed       -c $ctrl_bed      \\
   -f BED -n ${merge_name}_VS_Input --outdir $out_dir -g $ref -p $pvalue -B""")
-        
+
         return l_sh_info
-        
-        
+
+
     def s10_sortbdg(self):
         l_sh_info = []
         l_sh_info.append("sam=$1")
@@ -551,7 +556,7 @@ sh $sort_bdg_exe $dir/$sam/${sam}_VS_Input_treat_pileup.bdg                 \\
 sh $sort_bdg_exe $dir/$sam/${sam}_VS_Input_control_lambda.bdg               \\
     $dir/$sam/${sam}_VS_Input_control_lambda.sort.bdg""")
         return l_sh_info
-    
+
     def s11_makeIGV(self):
         l_sh_info = []
         l_sh_info.append("sam_in=$1")
@@ -573,16 +578,16 @@ fi
 $igvtools_exe toTDF                                                         \\
     $dir_in/$sam_in/${sam_in}_VS_Input_treat_pileup.bedGraph                \\
     $dir_out/${sam_out}/${sam_in}_treat_pileup.tdf   $genome_fa &&          \\
-    
+
 $igvtools_exe toTDF                                                         \\
     $dir_in/$sam_in/${sam_in}_VS_Input_control_lambda.bedGraph              \\
     $dir_out/${sam_out}/${sam_in}_control_lambda.tdf $genome_fa
-    
+
 rm  $dir_in/$sam_in/${sam_in}_VS_Input_treat_pileup.bedGraph                \\
     $dir_in/$sam_in/${sam_in}_VS_Input_control_lambda.bedGraph""")
-    
+
         return l_sh_info
-        
+
     def s11_makeIGV_broad(self):
         l_sh_info = []
         l_sh_info.append("sam_in=$1")
@@ -600,16 +605,16 @@ ln -s $dir_in/$sam_in/${sam_in}_control_lambda.bdg                          \\
 if [ ! -d $dir_out/${sam_out} ]
     then mkdir $dir_out/${sam_out}
 fi
-      
+
 $igvtools_exe toTDF $dir_in/$sam_in/${sam_in}_treat_pileup.bedGraph         \\
     $dir_out/${sam_out}/${sam_in}_treat_pileup.broadPeaks.tdf  $genome_fa &&\\
-    
+
 $igvtools_exe toTDF $dir_in/$sam_in/${sam_in}_control_lambda.bedGraph       \\
     $dir_out/${sam_out}/${sam_in}_control_lambda.broadPeaks.tdf $genome_fa
-    
+
 ln -s $dir_in/$sam_in/${sam_in}_peaks.broadPeak                             \\
     $dir_in/$sam_in/${sam_in}_peaks.broadPeak.bed
-    
+
 $igvtools_exe count $dir_in/$sam_in/${sam_in}_peaks.broadPeak.bed           \\
     $dir_out/${sam_out}/${sam_in}_peaks.broadPeak.tdf $genome_fa         && \\
 
@@ -617,8 +622,8 @@ rm  $dir_in/$sam_in/${sam_in}_treat_pileup.bedGraph                         \\
     $dir_in/$sam_in/${sam_in}_control_lambda.bedGraph                       \\
     $dir_in/$sam_in/${sam_in}_peaks.broadPeak.bed""")
         return l_sh_info
-        
-        
+
+
     def s12_PeakGeneRegion(self,
             TSS_genebody_up,TSS_genebody_down,TSS_promoter_up,
             TSS_promoter_down,ext_binlen=50,body_bincnt=100,
@@ -627,9 +632,9 @@ rm  $dir_in/$sam_in/${sam_in}_treat_pileup.bedGraph                         \\
         prefix  = ".".join(self.refGeneTxt.split(".")[:-1])
         genebody_region = "%s.up%d_down%d.%sBsorted.longestTid.bed"           %\
                      (prefix, TSS_genebody_up, TSS_genebody_down, "genebody.")
-        
+
         getbin_genebody_cpp = "%s/get_bin_ignore_peak/get_bin" % (self.bin)
-        
+
         l_sh_info = []
         l_sh_info.append("merge_name=$1")
         l_sh_info.append("dir_Peak_mrg=%s"       % self.dir_Peak_mrg      )
@@ -645,7 +650,7 @@ rm  $dir_in/$sam_in/${sam_in}_treat_pileup.bedGraph                         \\
         l_sh_info.append("ext_binlen=%d"         % ext_binlen             )
         l_sh_info.append("body_bincnt=%d"        % body_bincnt            )
         l_sh_info.append("tss_binlen=%d"         % tss_binlen             )
-        
+
         l_sh_info.append("""
 prefix_Peak=${dir_Peak_mrg}/${merge_name}/${merge_name}
 prefix_P=${dir_Peak_mrg_TSS}/${merge_name}/${merge_name}
@@ -662,13 +667,13 @@ zcat $count_bdg | tail -n +2                                               |\\
     $stat1 $stat2""")
 
         return l_sh_info
-        
-        
+
+
 
     def s13_RPM_density_rep(self):
         getbin_readcnt_cpp = "%s/chip01.bed_reads_unique/bed_read"% (self.bin)
         genome_fai = "%s.fai" % (self.genome_ref)
-        
+
         l_sh_info = []
         l_sh_info.append("brief_name=$1")
         l_sh_info.append("mapped_reads=$2")
@@ -680,27 +685,27 @@ zcat $count_bdg | tail -n +2                                               |\\
 unique_bed=${dir_bed_rep}/${brief_name}.sort.tagAlign.gz
 out_cnt=${dir_RPM_bins_rep}/${brief_name}/${brief_name}.cnt.uniq
 out_RPKM=${dir_RPM_bins_rep}/${brief_name}/${brief_name}.RPKM.uniq
-        
+
 zcat $unique_bed                                                           |\\
     $getbin_readcnt_cpp -b 100  $genome_fai /dev/stdin $out_cnt.100
-    
+
 awk '{ print $1*1000000*1000/(100 *'$mapped_reads') }' $out_cnt.100         \\
     >$out_RPKM.100
-    
+
 zcat $unique_bed                                                           |\\
     $getbin_readcnt_cpp -b 1000 $genome_fai /dev/stdin $out_cnt.1kb
-    
+
 awk '{ print $1*1000000*1000/(1000*'$mapped_reads') }' $out_cnt.1kb         \\
     >$out_RPKM.1kb""")
 
         return l_sh_info
 
-    
-        
+
+
     def s14_RPM_density_mrg(self):
         getbin_readcnt_cpp = "%s/chip01.bed_reads_unique/bed_read"% (self.bin)
         genome_fai = "%s.fai" % (self.genome_ref)
-        
+
         l_sh_info = []
         l_sh_info.append("merge_name=$1")
         l_sh_info.append("mapped_reads=$2")
@@ -712,21 +717,21 @@ awk '{ print $1*1000000*1000/(1000*'$mapped_reads') }' $out_cnt.1kb         \\
 unique_bed=${dir_bed_mrg}/${merge_name}.sort.tagAlign.gz
 out_cnt=${dir_RPM_bins_mrg}/${merge_name}/${merge_name}.cnt.uniq
 out_RPKM=${dir_RPM_bins_mrg}/${merge_name}/${merge_name}.RPKM.uniq
-        
+
 zcat $unique_bed                                                           |\\
     $getbin_readcnt_cpp -b 100  $genome_fai /dev/stdin $out_cnt.100
-    
+
 awk '{ print $1*1000000*1000/(100 *'$mapped_reads') }' $out_cnt.100         \\
     >$out_RPKM.100
-    
+
 zcat $unique_bed                                                           |\\
     $getbin_readcnt_cpp -b 1000 $genome_fai /dev/stdin $out_cnt.1kb
-    
+
 awk '{ print $1*1000000*1000/(1000*'$mapped_reads') }' $out_cnt.1kb         \\
     >$out_RPKM.1kb""")
 
         return l_sh_info
-        
+
 
     def s15_merge_RPKM(self):
         l_sh_info = []
@@ -756,7 +761,7 @@ rm ${prefix}_${window}.tmp""")
 
     def s16_densityBaselv(self):
         genome_fai = "%s.fai" % (self.genome_ref)
-        
+
         l_sh_info = []
         l_sh_info.append("sam=$1")
         l_sh_info.append("ref=$2")
@@ -793,7 +798,7 @@ $ucsc_dir/bigWigAverageOverBed                                              \\
     /dev/stdout | awk '{print $6}' >$dir/$sam/${sam}.1kb.norm_avg.xls
 
 """)
-        
+
         return l_sh_info
 
     def s17_merge_RPKM(self):
